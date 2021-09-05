6e51882695822c7f65f78490870b236c5da43ce5
saksham singh <saksham.webdev@gmail.com>
2021-09-05 19:21:30 +0530

    website

---
+++ js/music-list.js
@@ -0,0 +1,185 @@
+
+let allMusic = [
+  {
+    name: "Aaj Se Teri sari Galliya",
+    artist: "Arijeet Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Nachange Saari Raat",
+    artist: " Tulsi Kumar, Meet Bros",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Na Jaane Kya Hai Tumse Waasta",
+    artist: "Jubin Nautiyal",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Paniyon Sa",
+    artist: "Atif Aslam, Tulsi Kumar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Party On My Mind ",
+    artist: " Shefali Alvares, Yo Yo Honey Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Rula Ke Gaya Ishq",
+    artist: " Sunny Inder, Stebin Ben",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "TERE JISM 2",
+    artist: "Altaaf Sayyed",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Mast Magan",
+    artist: "Arijit Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Ijazat",
+    artist: "Arijit Singh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Mujhko Barsaat Bana Lo",
+    artist: "Armaan Malik",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Bharat: Slow Motion",
+    artist: "Shreya Ghoshal, Nakash Aziz",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Naino Ki Jo Baat Naina Jaane hai",
+    artist: "Altaaf Sayyed",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Sunn Raha Hai Na Tu ",
+    artist:  "Ankit Tiwari",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Naino Ne Bandhi ",
+    artist:  "Yasser Desai",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Illegal Weapon 2.0 ",
+    artist:  "Garry Sandhu, Jasmine Sandlas",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Tera Hone Laga Hoon  ",
+    artist:  " Alisha Chinai, Atif Aslam, Joi Barua",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Pyar Ki Full Video Song  ",
+    artist:  "Toshi Sabri, Anmol Malik",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Tera Fitoor",
+    artist:  " Arijit Singh",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Tu Hi Hai Aashiqui  ",
+    artist:  "Arijit Singh, Pallal Muchhal",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Main Rang Sharbaton Ka ",
+    artist:  "Atif Aslam, Chinmayi",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Ishq Da Sutta ",
+    artist:  "Meet Bros",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Pyar me aksar",
+    artist:  "Sampreet Dutta",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Wajah Tum Ho  ",
+    artist:  "Aramaan Malik",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Fakira ",
+    artist:  "Vishal–Shekhar, Neeti Mohan",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "Ya Ali Madad Wali  ",
+    artist:  "Zubeen Garg",
+    artist:  "Atif Aslam, Chinmayi",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "Malang",
+    artist:  "Shilpa Rao, Siddharth Mahadevan",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Boond Boond ",
+    artist:  " Jubin Nautiyal, Neeti Mohan",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "Makhna ",
+    artist:  "Tanishk Bagchi, Asees Kaur",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "Lo safar ",
+    artist:  "Jubin Nautiyal",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "Don't Be Shy Again ",
+    artist:  "Gurdeep Mehndi, Shalmali Kholgade",
+    img: "music-30",
+    src: "music-30"
+  },
+
+];
